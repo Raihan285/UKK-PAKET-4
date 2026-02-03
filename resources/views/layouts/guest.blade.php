23edc57cdb584f6a407998b0234f71b5f32529cc
Raihan285 <raihaniyang95@gmail.com>
2026-02-03 11:29:14 +0800

    add guest file

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Booktify</title>
+    @vite('resources/css/app.css') 
+</head>
+<body class="bg-gray-100 h-screen flex items-center justify-center">
+    @yield('content')
+</body>
+</html>
